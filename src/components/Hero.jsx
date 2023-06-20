eb6ce0c3c138ec5eb6b9ff4b26d9271f9899a884
ᴅ ɪ ɪ <aodirizky05@gmail.com>
2023-06-20 12:15:10 +0700

    Initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,6 +34,8 @@ const Hero = () => {
           <p>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.
             We examine annual percentage rates, annual fees.</p>
         </div>
+
+        <p>test</p>
       </div>
     </section>
   )
