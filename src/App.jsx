11266999ce33c21938a3848cbd0e225b6cf79f32
ᴅ ɪ ɪ <aodirizky05@gmail.com>
2023-08-13 19:30:45 +0800

    responsive fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
 import React from 'react'
+
 import styles from './styles/styles.js'
 import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Hero, Stats, Testimonials } from './components/components'
 
 const App = () => {
   return (
-    <div className='bg-primary w-full h-full overflow-clip py-4'>
-      <div id='home'>
-        <div className={`${styles.paddingX} ${styles.flexCenter}}`}>
-          <Navbar />
+    <div className='bg-primary'>
+      <div className='max-w-[1920px] m-auto'>
+        <div className={`${styles.paddingX}}`}>
+          <div id='home'>
+            <Navbar />
+          </div >
         </div>
 
         <div className={`${styles.paddingX}`}>
           <Hero />
           <Stats />
         </div>
+
+        <div className={`${styles.paddingX}`}>
+          <div id='features'>
+            <Business />
+          </div>
+
+          <div id='product'>
+            <Billing />
+            <CardDeal />
+          </div>
+
+          <div id='clients'>
+            <Testimonials />
+            <Clients />
+            <CTA />
+            <Footer />
+          </div>
+        </div>
       </div>
-
-      <div className={`${styles.paddingX}`}>
-        <div id='features'>
-          <Business />
-        </div>
-
-        <div id='product'>
-          <Billing />
-          <CardDeal />
-        </div>
-
-        <div id='clients'>
-          <Testimonials />
-          <Clients />
-          <CTA />
-          <Footer />
-        </div>
-      </div>
-    </div >
+    </div>
   )
 }
 
